bc0664af2a486dcb3cc7f95fa1b0f97ebaf6068a
Benjamin Krueger <ben.krueger@expeditors.com>
2016-06-15 20:28:38 -0700

    Fixed prompt

--- .shellrc/bashrc.d/env.sh
+++ .shellrc/bashrc.d/env.sh
@@ -1,1 +1,13 @@
-export PS1='\u@\h \w \$ '
+#export PS1='\u@\h \w \$ '
+
+export LC_TYPE="C.utf8"
+
+rightprompt()
+{
+    printf "%*s" $COLUMNS $?
+}
+
+#if [[ $LC_CTYPE =~ \.[Uu][Tt][Ff]-?8 ]]; then
+#  export PS1=$'$? \e[0;32m\xce\xbb\x0a\e[0m '
+  export PS1=$'$(rightprompt)\e[0;32m\u03bb\e[0m '
+#fi
