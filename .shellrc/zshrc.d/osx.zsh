382593deac44f667467ab27d6ed4c7ec140c9a12
Benjamin Krueger <benjamin@seattlefenix.net>
2017-06-29 01:11:54 -0700

    Adding a couple useful functions

--- .shellrc/zshrc.d/osx.zsh
+++ .shellrc/zshrc.d/osx.zsh
@@ -4,4 +4,6 @@ if [ `uname $UNAME_OPT` = Darwin ]
 then
   fwlog () {sudo /usr/sbin/tcpdump -vlnettti pflog0}
   growl () {echo -e $'\e]9;'${1}'\007' ; return  ;}
+  doc () { open dash://$* }
+  dns-traffic () { tshark -Y "dns.flags.response == 1" -Tfields -e frame.time_delta -e dns.qry.name -e dns.a -Eseparator=/t -Eheader=y $*}
 fi
