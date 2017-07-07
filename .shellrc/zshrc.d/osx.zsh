452da8fa26a58c31403b1a871af7ff530f0c8cff
Benjamin Krueger <benjamin@seattlefenix.net>
2017-07-06 18:48:00 -0700

    Added path for e2fsprogs

--- .shellrc/zshrc.d/osx.zsh
+++ .shellrc/zshrc.d/osx.zsh
@@ -6,4 +6,7 @@ then
   growl () {echo -e $'\e]9;'${1}'\007' ; return  ;}
   doc () { open dash://$* }
   dns-traffic () { tshark -Y "dns.flags.response == 1" -Tfields -e frame.time_delta -e dns.qry.name -e dns.a -Eseparator=/t -Eheader=y $*}
+
+ export PATH="/usr/local/opt/e2fsprogs/bin:$PATH"
+ export PATH="/usr/local/opt/e2fsprogs/sbin:$PATH" 
 fi
