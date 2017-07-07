dd671ad650223a49423f494552d5003c6e0bd4d4
Benjamin Krueger <benjamin@seattlefenix.net>
2017-07-06 22:45:38 -0700

    Nitpick, fixing indent

--- .shellrc/zshrc.d/osx.zsh
+++ .shellrc/zshrc.d/osx.zsh
@@ -7,6 +7,6 @@ then
   doc () { open dash://$* }
   dns-traffic () { tshark -Y "dns.flags.response == 1" -Tfields -e frame.time_delta -e dns.qry.name -e dns.a -Eseparator=/t -Eheader=y $*}
 
- export PATH="/usr/local/opt/e2fsprogs/bin:$PATH"
- export PATH="/usr/local/opt/e2fsprogs/sbin:$PATH" 
+  export PATH="/usr/local/opt/e2fsprogs/bin:$PATH"
+  export PATH="/usr/local/opt/e2fsprogs/sbin:$PATH" 
 fi
